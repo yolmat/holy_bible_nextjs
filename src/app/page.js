720394804041c70ea810db8c713c1009f2aee779
Mateus <mateus.saraiva09@gmail.com>
2023-09-05 23:55:24 -0300

    Create a home page and styled

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,31 @@
-import { TitleLogin, MainLogin } from "./style"
+import Link from "next/link"
+import { TitleHome, MainHome, SubtitleHome, ButtonsHome, ButtonHomeLink } from "./style"
 
 export default function Home() {
+
   return (
-    <MainLogin>
-      <TitleLogin>Biblia</TitleLogin>
-      <h2>O livro sobre Amor</h2>
-    </MainLogin>
+    <MainHome
+      initial={{ opacity: 0, scale: 0.2 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 1.5,
+        delay: 0.2,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}>
+      <TitleHome>Biblia</TitleHome>
+      <SubtitleHome>O livro sobre Amor</SubtitleHome>
+      <ButtonsHome>
+        <Link href="/Login">
+          <ButtonHomeLink
+            whileHover={{ scale: 1.04 }} whileTap={{ scale: 1 }}
+          >Login</ButtonHomeLink>
+        </Link>
+        <Link href="/Singup">
+          <ButtonHomeLink
+            whileHover={{ scale: 1.04 }} whileTap={{ scale: 1 }}
+          >Cadastre-se</ButtonHomeLink>
+        </Link>
+      </ButtonsHome>
+    </MainHome >
   )
 }
--- src/app/style.js
+++ src/app/style.js
@@ -1,23 +1,9 @@
 'use client'
 
-import styled, { keyframes } from 'styled-components'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
-const animation = keyframes`
-    0%{
-        top: 0
-    }
-    
-    50%{
-        top: 50
-    }
-
-    100%{
-        top: 100
-    }
-`
-
-export const MainLogin = styled.main`
-
+export const MainHome = styled(motion.div)`
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
@@ -25,9 +11,32 @@ export const MainLogin = styled.main`
     height: 100vh;
 `
 
-export const TitleLogin = styled.h1`
-    position: relative;
-    animation: ${animation} 2s linear;
-    
+export const TitleHome = styled.h1`
+    color: #c5c5c5;
+    font-size: 4rem;
+`
 
+export const SubtitleHome = styled.h2`
+    color: #a9a9a9;
+    font-size: 2rem;
+    margin: 15px 0 20px 0;
+`
+
+export const ButtonsHome = styled.div`
+    display:flex;
+    flex-flow: row nowrap;
+    gap: 1rem;
+`
+
+export const ButtonHomeLink = styled(motion.button)`
+    width: 10rem;
+    height: 3rem;
+    cursor: pointer;
+
+    border:none; 
+    border-radius: 15px;
+    font-size: 1rem;
+    background-color: #565656;
+    color: #fff;
+    font-weight: bold;
 `
